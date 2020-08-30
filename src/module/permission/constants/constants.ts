09d67cd76223800591f4cac333bd11c4cf53bd67
Artem Ilinykh <devsinglesly@gmail.com>
2020-08-30 15:08:25 +0600

    constants

---
+++ src/module/permission/constants/constants.ts
@@ -0,0 +1,4 @@
+export const PACKAGE_NAME = "@reactive-underground/permissions";
+
+export const ROLE_REPOSITORY_INTERFACE = `${PACKAGE_NAME}/repository/RoleRepositoryInterface`;
+export const PERMISSION_REPOSITORY_INTERFACE = `${PACKAGE_NAME}/repository/PermissionRepositoryInterface`;
