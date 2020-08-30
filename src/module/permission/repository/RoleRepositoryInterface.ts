66e231ab02a43416a795d54e482bd10ce8de5a8b
Artem Ilinykh <devsinglesly@gmail.com>
2020-08-30 15:03:09 +0600

    repository interfaces

---
+++ src/module/permission/repository/RoleRepositoryInterface.ts
@@ -0,0 +1,11 @@
+import { Role } from "~/module/permission/entity/Role";
+
+export interface RoleRepositoryInterface {
+    findAll(): Promise<Role[]>;
+    findByIds(ids: number[]): Promise<Role[]>;
+    save(role: Role): Promise<void>;
+    update(role: Role): Promise<void>;
+    removeById(id: number): Promise<void>
+    getOneById(id: number): Promise<Role>
+    findByName(name: string): Promise<Role|undefined>;
+}
