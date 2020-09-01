350f122200cb05a12ed3f7f983f5509c48dcf805
Artem Ilinykh <devsinglesly@gmail.com>
2020-09-01 15:11:31 +0600

    remove unused interface

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@
  * @author Artem Ilinykh devsinglesly@gmail.com
  */
 export * from "./module/permission/PermissionModule";
-export * from "./module/permission/interface/PermissionInterface";
 export * from "./module/permission/interface/PermissionSubjectInterface";
 export * from "./module/permission/entity/Role";
 export * from "./module/permission/entity/Permission";
